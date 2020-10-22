393473afd99f910932f10f03f0d2f515e1bf003a
Dragon <53557855+000bakhtiyor@users.noreply.github.com>
2020-10-22 09:04:47 +0500

    Update main.py

--- main.py
+++ main.py
@@ -2,17 +2,17 @@ from pyrogram import Client
 from pyrogram.raw import functions
 from time import sleep
 
-api_id = 516739
-api_hash = "3dde907b8c393ff9d60719e5d1bb7f38"
+api_id =                                                 # api_id  
+api_hash =                                               # api_hash
 
 app=  Client("my_account",api_id,api_hash)
 app.start()
-count = app.get_chat_members_count(-1001443604870)
+count = app.get_chat_members_count("get_member_Group_id")
 print(count)
 for i in range(count):
-    id = app.get_chat_members(-1001443604870)[i].user
+    id = app.get_chat_members("get_member_Group_id")[i].user
 
-    app.add_chat_members('testpythonbots', id.id)
+    app.add_chat_members('add_member_Group_id', id.id)
 
 
 
